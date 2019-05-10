cebf257f14d27a620bd5a47f38f61afa4644babd
马嘉远 <944414275@qq.com>
2019-05-10 09:45:47 +0800

    1

--- convertTest/Program.cs
+++ convertTest/Program.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Configuration;
+using System.Threading;
 
 namespace convertTest
 {
     class Program
     {
+        static string _appTime;
+
+
         static void Main(string[] args)
         {
+            int _dateTime;
+            _appTime = ConfigurationManager.AppSettings["time"];
+            _dateTime=Convert.ToInt32(_appTime);
+            //Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
